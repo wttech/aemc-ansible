1688fd8000af94d13c110c8fb22321b64de32405
Krystian Panek <krystian.panek@wundermanthompson.com>
2023-03-09 14:42:46 +0100

    Packer example hardening

--- examples/packer/roles/aem_instance/templates/aem.sh
+++ examples/packer/roles/aem_instance/templates/aem.sh
@@ -4,34 +4,5 @@
 
 # TODO export AEM_INSTANCE_LOCAL_SERVICE_MODE=true
 
-aem_start() {
-  su - "{{ aem_system_user }}" -c ". /etc/profile && cd {{ aem_root_dir | dirname }} && {{ aem_cli_executable }} instance start"
-}
-
-aem_stop() {
-  su - "{{ aem_system_user }}" -c ". /etc/profile && cd {{ aem_root_dir | dirname }} && {{ aem_cli_executable }} instance stop"
-}
-
-aem_status() {
-  su - "{{ aem_system_user }}" -c ". /etc/profile && cd {{ aem_root_dir | dirname }} && {{ aem_cli_executable }} instance status"
-}
-
-case "$1" in
-  start)
-    aem_start
-  ;;
-  stop)
-    aem_stop
-  ;;
-  restart)
-    aem_stop
-    aem_start
-  ;;
-  status)
-    aem_status
-  ;;
-  *)
-  echo "Usage: {start|stop|restart|status}"
-  exit 1
-  ;;
-esac
+COMMAND="'$*'"
+su - "{{ aem_system_user }}" -c ". /etc/profile && cd {{ aem_root_dir | dirname }} && {{ aem_cli_executable }} ${COMMAND}"
