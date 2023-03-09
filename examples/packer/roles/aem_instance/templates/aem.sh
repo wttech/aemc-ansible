5a6659ccf60ad829c97668b0e9fab7d350901275
Krystian Panek <krystian.panek@wundermanthompson.com>
2023-03-09 18:48:22 +0100

    AEM system user script / passthrough

--- examples/packer/roles/aem_instance/templates/aem.sh
+++ examples/packer/roles/aem_instance/templates/aem.sh
@@ -3,6 +3,6 @@
 {{ ansible_managed | comment }}
 
 # TODO export AEM_INSTANCE_LOCAL_SERVICE_MODE=true
+# TODO export AEM_OUTPUT_LOG_MODE=both
 
-COMMAND="'$*'"
-su - "{{ aem_system_user }}" -c ". /etc/profile && cd {{ aem_root_dir | dirname }} && {{ aem_cli_executable }} ${COMMAND}"
+su - "{{ aem_system_user }}" -c ". /etc/profile && cd {{ aem_root_dir | dirname }} && {{ aem_cli_executable }} $*"
